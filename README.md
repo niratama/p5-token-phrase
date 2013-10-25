4c1eb5bb6d3099e22fbf06ab120bcc97ecd4d835
Kenichi Kobayashi <niratama3@gmail.com>
2013-10-25 21:45:37 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Text::TokenPhrase - It's new $module
+
+# SYNOPSIS
+
+    use Text::TokenPhrase;
+
+# DESCRIPTION
+
+Text::TokenPhrase is ...
+
+# LICENSE
+
+Copyright (C) Kenichi Kobayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kenichi Kobayashi <niratama3@gmail.com>
---
+++ lib/Text/TokenPhrase.pm
@@ -0,0 +1,39 @@
+package Text::TokenPhrase;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Text::TokenPhrase - It's new $module
+
+=head1 SYNOPSIS
+
+    use Text::TokenPhrase;
+
+=head1 DESCRIPTION
+
+Text::TokenPhrase is ...
+
+=head1 LICENSE
+
+Copyright (C) Kenichi Kobayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kenichi Kobayashi E<lt>niratama3@gmail.comE<gt>
+
+=cut
+
